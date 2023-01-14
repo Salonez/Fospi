d98666269b4db11922a09a34ab3cb1f53331bf30
Tyrone Aquino-Salomon <tyroneaquinosalomon@gmail.com>
2023-01-14 20:47:42 +1100

    Finally implemented draft DietaryNeeds page frontend

--- client/src/pages/DietaryNeeds.js
+++ client/src/pages/DietaryNeeds.js
@@ -7,6 +7,15 @@ export default function DietaryNeeds() {
     // Set up what diets the user is on and tick those boxes
     const [diets, setDiets] = useState([]);
     const allDiets = ["vegetarian", "pescatarian", "vegan", "lowFibre"];
+    let checks = [];
+
+    // function buildCheckBox(diet) {
+    //     if (diets.includes(diet)) {
+    //         checks.push(React.createElement("checkbox", {checked: true}, "{diet}"));
+    //     } else {
+    //         checks.push(React.createElement("checkbox", {}, "{diet}"));
+    //     }
+    // }
 
     useEffect(() => { 
         fetch('http://127.0.0.1:8000/DietaryNeeds', {
@@ -19,6 +28,9 @@ export default function DietaryNeeds() {
             })
         }).then(response => response.json())
         .then(json => setDiets(json['diets']));
+        // .then(() => {
+        //     allDiets.forEach(buildCheckBox);
+        // })
     }, []);
 
     return (
@@ -26,11 +38,13 @@ export default function DietaryNeeds() {
             <Navbar />
             <div>
                 <h1>Current diets:</h1>
-                {allDiets.map(diet => {
-                    if (diets.includes(diet))
-                        return <pre><input type="checkbox" checked/>{diet}</pre>;
-                    return <pre><input type="checkbox"/>{diet}</pre>;
-                })}
+                    <form>
+                        {allDiets.map(diet => {
+                            if (diets.includes(diet))
+                                return <pre><input type="checkbox" defaultChecked={true}/>{diet}</pre>;
+                            return <pre><input type="checkbox"/>{diet}</pre>;
+                        })}
+                    </form>
             </div>
         </div>
     )
